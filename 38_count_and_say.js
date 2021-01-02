ac7ae5eb2e035b2e80593b205a788593518eca2b
asd0999 <obelixotic@gmail.com>
2021-01-01 19:42:54 -0500

    problem #38 - count and say

--- 38_count_and_say.js
+++ 38_count_and_say.js
@@ -3,34 +3,58 @@
  * @return {string}
  */
 var countAndSay = function(n) {
-    // if (n == 1) return "1";
-    // for (let t = 0; t < array.length; t++) {}
-    let strArr = n.toString().split("");
+    if (n == 1) return "1";
     let cur;
-    let prev = strArr[0];
-    let count = 1;
     let answer = "";
-    for (let i = 1; i < strArr.length; i++) {
-        cur = strArr[i];
-        if (cur == prev) {
-            count++;
-        } else {
-            answer += count.toString();
-            answer += strArr[i - 1];
-            // console.log(answer);
-            // console.log(count);
-            count = 1;
+    let prev;
+    let strArr = "1";
+    let count = 1;
+    let times = 1;
+    while (times < n) {
+        // console.log("\nn = ", times + 1);
+        // console.log(strArr.split(""));
+        prev = "";
+        answer = "";
+        count = 1;
+        for (let i = 0; i < strArr.length; i++) {
+            if (strArr.length == 1) {
+                break;
+            }
+            cur = strArr[i];
+            // console.log("cur: ", cur);
+            // console.log("prev: ", prev);
+            if (cur == prev) {
+                count++;
+                // console.log("Count++", count);
+                lastStrAdded = false;
+            } else if (prev != "") {
+                // console.log("Count: ", count);
+                answer += count.toString();
+                answer += strArr[i - 1];
+                // console.log("Answer: ", answer);
+                count = 1;
+            }
+            prev = strArr[i];
         }
-        prev = strArr[i];
+
+        answer += count.toString();
+        answer += strArr[strArr.length - 1];
+        // console.log("Final Answer: ", answer);
+
+        strArr = answer;
+        times++;
     }
-    answer += count.toString();
-    answer += strArr[strArr.length - 1];
-    // console.log(count);
+
     return answer;
 };
 
+// check :
 console.log(countAndSay(1));
-console.log(countAndSay(11));
-console.log(countAndSay(21));
-console.log(countAndSay(1211));
-console.log(countAndSay(111221));
+console.log(countAndSay(2));
+console.log(countAndSay(3));
+console.log(countAndSay(4));
+console.log(countAndSay(5));
+console.log(countAndSay(6));
+console.log(countAndSay(7));
+console.log(countAndSay(8));
+console.log(countAndSay(9));
