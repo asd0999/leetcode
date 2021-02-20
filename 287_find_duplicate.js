d2527ed796053156a89fa39b8abb811a40230cf4
asd0999 <obelixotic@gmail.com>
2021-02-20 18:33:07 -0500

    #287 - find duplicate challenged solved by floyds hare and tortoise algorithm

---
+++ 287_find_duplicate.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function(nums) {
+    let tortoise = nums[0];
+    let hare = nums[0];
+
+    while (true) {
+        tortoise = nums[tortoise];
+        hare = nums[nums[hare]];
+        if (tortoise === hare) {
+            break;
+        }
+    }
+
+    let ptr1 = nums[0];
+    let ptr2 = tortoise;
+    while (ptr1 !== ptr2) {
+        ptr1 = nums[ptr1];
+        ptr2 = nums[ptr2];
+    }
+
+    return ptr1;
+};
