91d54255aab32037a11d237d87cf21f24a2580c5
asd0999 <obelixotic@gmail.com>
2021-02-23 19:43:49 -0500

    cant be solved like this, need to use BFS

--- 854_k_similar_strings.js
+++ 854_k_similar_strings.js
@@ -22,13 +22,11 @@ var kSimilarity = function(A, B) {
                     bword[ii] === A[i] &&
                     A[ii] === bword[i]
                 ) {
-                    // console.log(i, ii);
                     skips.add(i);
                     skips.add(ii);
                     return ii;
                 }
             });
-            // console.log(index);
             if (index !== -1) {
                 let tmp = bword[index];
                 bword[index] = bword[i];
@@ -37,39 +35,62 @@ var kSimilarity = function(A, B) {
             }
         }
     }
-    console.log("swaps so far:", swaps);
+    // console.log("swaps so far:", swaps);
+    // console.log(A);
+    // console.log(bword.join(""));
 
-    console.log(A);
-    console.log(bword.join(""));
+    // for (let i = 0; i < A.length; i++) {
+    //     if (A[i] !== bword[i] && !skips.has(i)) {
+    //         index = bword.findIndex((char, ii) => {
+    //             if (char === A[i] && ii >= i && char === A[bword.indexOf(A[ii])]) {
+    //                 // skips.add(i);
+    //                 // skips.add(ii);
+    //                 return ii;
+    //                 // }
+    //             }
+    //         });
+    //         // if (index !== -1) {
+    //         // console.log(i, index);
+    //         let tmp = bword[index];
+    //         bword[index] = bword[i];
+    //         bword[i] = tmp;
+    //         swaps++;
+    //         // }
+
+    //         // console.log(bword.join(""));
+    //     }
+    // }
 
     for (let i = 0; i < A.length; i++) {
         if (A[i] !== bword[i] && !skips.has(i)) {
             index = bword.findIndex((char, ii) => {
                 if (char === A[i] && ii > i) {
-                    // console.log(i, ii);
-                    skips.add(ii);
+                    // console.log(i, index, char, A[i]);
+                    // skips.add(i);
+                    // skips.add(ii);
                     return ii;
                 }
             });
-            // console.log(index);
-            let tmp = bword[index];
-            bword[index] = bword[i];
-            bword[i] = tmp;
-            swaps++;
+            if (index !== -1) {
+                let tmp = bword[index];
+                bword[index] = bword[i];
+                bword[i] = tmp;
+                swaps++;
+            }
 
             // console.log(bword.join(""));
         }
     }
-    console.log(skips);
+    // console.log(skips);
     return swaps;
 };
 
 // test :
-// console.log(kSimilarity("ab", "ba")); // 1
-// console.log(kSimilarity("abc", "bca")); // 2
-// console.log(kSimilarity("abac", "baca")); // 2
-// console.log(kSimilarity("aabc", "abca")); // 2
-// console.log(kSimilarity("abcbca", "ababcc")); // 1
-// console.log(kSimilarity("abcdeabcdeabcdeabcde", "aaaabbbbccccddddeeee")); // 8
-// console.log(kSimilarity("aabbccddee", "cdacbeebad")); // 6
+console.log(kSimilarity("ab", "ba")); // 1
+console.log(kSimilarity("abc", "bca")); // 2
+console.log(kSimilarity("abac", "baca")); // 2
+console.log(kSimilarity("aabc", "abca")); // 2
+console.log(kSimilarity("abcbca", "ababcc")); // 1
+console.log(kSimilarity("abcdeabcdeabcdeabcde", "aaaabbbbccccddddeeee")); // 8
+console.log(kSimilarity("aabbccddee", "cdacbeebad")); // 6
 console.log(kSimilarity("abccaacceecdeea", "bcaacceeccdeaae")); // 9
