3a7d411764f299b513ec604f70d9c7e4126c0d0a
asd0999 <obelixotic@gmail.com>
2021-02-24 15:42:44 -0500

    #144 - BT preorder traversal - iterative solution

--- 144_BT_preorder_traversal.js
+++ 144_BT_preorder_traversal.js
@@ -24,3 +24,18 @@ const helper = (root, res) => {
     helper(root.left, res);
     helper(root.right, res);
 };
+
+// iterative solution
+var preorderTraversal = function(root) {
+    if (!root) return [];
+    let res = [];
+    let stack = [root];
+    let node = null;
+    while (stack.length > 0) {
+        node = stack.pop();
+        res.push(node.val);
+        if (node.right) stack.push(node.right);
+        if (node.left) stack.push(node.left);
+    }
+    return res;
+};
