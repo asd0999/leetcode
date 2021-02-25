57fea5d4f0b1d8aaeb880bdd8780951dcf910f13
asd0999 <obelixotic@gmail.com>
2021-02-24 23:39:47 -0500

    #102 - BT level order traversal - only recursive solution to this problem

---
+++ 102_BT_level_order_traversal.js
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+// solution 1 - i understand
+var levelOrder = function(root) {
+    if (!root) return [];
+    return helper([
+        [root]
+    ], 0);
+};
+
+var helper = function(res, level) {
+    var now = res[level];
+    var next = [];
+
+    for (var i = 0; i < now.length; i++) {
+        if (now[i].left) next.push(now[i].left);
+        if (now[i].right) next.push(now[i].right);
+        now[i] = now[i].val;
+    }
+
+    if (next.length) {
+        res.push(next);
+        helper(res, level + 1);
+    }
+
+    return res;
+};
+
+// solution 2 - i dont understand
+var levelOrder = function(root) {
+    return helper([], root, 0);
+};
+
+var helper = function(res, root, level) {
+    if (root) {
+        if (!res[level]) res[level] = [];
+        res[level].push(root.val);
+        helper(res, root.left, level + 1);
+        helper(res, root.right, level + 1);
+    }
+    return res;
+};
+
+// ref: https://baffinlee.com/leetcode-javascript/problem/binary-tree-level-order-traversal.html
