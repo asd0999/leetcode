499f5a2d71f5b6732e76ec62c6b4587c603b34d2
asd0999 <obelixotic@gmail.com>
2021-02-25 16:55:34 -0500

    sukanya sent gist

--- 3_longest_substring.js
+++ 3_longest_substring.js
@@ -5,37 +5,54 @@
 var lengthOfLongestSubstring = function(s) {
     if (!s) return 0;
 
-    let m = {};
-    for (let i = 0; i < s.length; i++) {
-        m[i] = s[i];
-    }
-    let cur = m[0];
-    let g_max = 1;
-    let l_max = 1;
-    console.log(m);
+    let set = new Set();
+    let g_max = 0;
+    let l_max = 0;
+    let cur = "";
 
-    for (let i = 1; i < Object.keys(m).length; i++) {
-        console.log(i);
-        if (m[i] !== m[i - 1] && !cur.includes(m[i])) {
+    for (let i = 0; i < s.length; i++) {
+        // console.log("0", set);
+        cur = s[i];
+        if (!set.has(cur)) {
+            set.add(cur);
             l_max++;
-            cur += m[i];
-            if (l_max > g_max) g_max = l_max;
-        } else {
-            let ii = parseInt(Object.keys(m).find((key) => m[key] === m[i]));
-            console.log("else", ii, i);
-            i = ii + 1;
-            cur = m[i + 1];
-            console.log("cur", cur);
-            l_max = 1;
-            continue;
+            if (l_max > g_max) {
+                g_max = l_max;
+            }
         }
-        // console.log(cur, l_max, g_max);
+        // console.log("1", set);
+        for (let j = i + 1; j < s.length; j++) {
+            if (s[i] !== s[j] && !set.has(s[j])) {
+                l_max++;
+                set.add(s[j]);
+                // console.log("2", set);
+            } else {
+                if (l_max > g_max) {
+                    g_max = l_max;
+                }
+                l_max = 0;
+                while (i < j) {
+                    if (s[i] === s[j]) {
+                        set.delete(s[i]);
+                        // console.log("3", set);
+                        break;
+                    }
+                    i++;
+                }
+                // console.log("4", set);
+            }
+            if (l_max > g_max) {
+                g_max = l_max;
+            }
+        }
     }
     return g_max;
 };
 
-// console.log(lengthOfLongestSubstring("abcabcbb")); //3
-// console.log(lengthOfLongestSubstring("bbbb")); //1
-// console.log(lengthOfLongestSubstring("pwwkew")); //3
-// console.log(lengthOfLongestSubstring("dvdf")); //3
+// test :
+console.log(lengthOfLongestSubstring("abcabcbb")); //3
+console.log(lengthOfLongestSubstring("adbcb")); //4
+console.log(lengthOfLongestSubstring("bbbb")); //1
+console.log(lengthOfLongestSubstring("pwwkew")); //3
+console.log(lengthOfLongestSubstring("dvdf")); //3
 console.log(lengthOfLongestSubstring("anviaj")); //5
