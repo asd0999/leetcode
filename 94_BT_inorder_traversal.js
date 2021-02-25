196a20a210a706b5983275d5a537901a2e768cd3
asd0999 <obelixotic@gmail.com>
2021-02-24 22:32:47 -0500

    #94 - BT inorder traversal - iterative solution

--- 94_BT_inorder_traversal.js
+++ 94_BT_inorder_traversal.js
@@ -24,3 +24,25 @@ var helper = function(root, res) {
     res.push(root.val);
     helper(root.right, res);
 };
+
+// iterative solution
+var inorderTraversal = function(root) {
+    if (!root) return [];
+    let res = [];
+    let stack = [];
+    let node = root;
+    while (true) {
+        if (node !== null) {
+            stack.push(node);
+            node = node.left;
+        } else {
+            if (stack.length === 0) {
+                break;
+            }
+            node = stack.pop();
+            res.push(node.val);
+            node = node.right;
+        }
+    }
+    return res;
+};
