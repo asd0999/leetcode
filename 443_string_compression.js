038dad4e9c1ff24275d24f3079b05b9ab214a279
asd0999 <obelixotic@gmail.com>
2021-02-26 16:33:58 -0500

    #443 - string compression

--- 443_string_compression.js
+++ 443_string_compression.js
@@ -9,20 +9,21 @@ var compress1 = function(chars) {
     return comp.split("");
 };
 // test:
-console.log(compress1("aaaabb"));
+// console.log(compress1("aaaabb"));
+// console.log(compress1(["a", "a", "b", "b", "c", "c", "c"]));
 
 // solution with array iteration
 var compress = function(chars) {
     const res = [];
     let count = 1;
     for (let i = 0; i < chars.length; i++) {
-        if (chars[i] === chars[i - 1]) continue;
+        // if (chars[i] === chars[i - 1]) continue;
         let index = i;
         while (i > 0 && chars[i] === chars[i + 1]) {
             count++;
             i++;
         }
-        i = index;
+        i = index + count;
         res.push(chars[i]);
         if (count > 0) res.push(count + 1);
         count = 0;
@@ -33,4 +34,4 @@ var compress = function(chars) {
 console.log(compress(["a", "a", "b", "b", "c", "c", "c"]));
 console.log(compress(["a", "a", "b", "c", "c", "c"]));
 
-// turns out leetcdeo doesnt want to accept any of my answers
+// turns out leetcode doesnt want to accept any of my answers, i dont understand why all my returns show up as [] when a console.log right before shows the right answer
