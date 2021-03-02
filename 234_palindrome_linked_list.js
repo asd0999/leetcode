368f986fb56a08a28bcdd8b35af5593a0823e733
asd0999 <obelixotic@gmail.com>
2021-03-02 00:14:43 -0500

    #234 - palindrome linked list, optimized solution 3 (faster than 97%)

--- 234_palindrome_linked_list.js
+++ 234_palindrome_linked_list.js
@@ -10,35 +10,36 @@
  * @return {boolean}
  */
 var isPalindrome = function(head) {
-    //     //original linked list
-    //     let original = [];
-    //     let cur = head;
-    //     while (cur != null) {
-    //         original.push(cur.val);
-    //         cur = cur.next;
-    //     }
-    //     // console.log(original);
+    // solution 1 - push val to array, reverse linked list and push val to another array and compare the two arrays
+    //original linked list
+    let original = [];
+    let cur = head;
+    while (cur != null) {
+        original.push(cur.val);
+        cur = cur.next;
+    }
+    // console.log(original);
 
-    //     //reverse linked list
-    //     let reverse = [];
-    //     cur = head;
-    //     let next = null;
-    //     let prev = null;
-    //     while (head) {
-    //         next = head.next;
-    //         head.next = prev;
-    //         prev = head;
-    //         head = next;
-    //     }
+    //reverse linked list
+    let reverse = [];
+    cur = head;
+    let next = null;
+    let prev = null;
+    while (head) {
+        next = head.next;
+        head.next = prev;
+        prev = head;
+        head = next;
+    }
 
-    //     cur = prev;
-    //     while (cur != null) {
-    //         reverse.push(cur.val);
-    //         cur = cur.next;
-    //     }
-    //     // console.log(reverse);
+    cur = prev;
+    while (cur != null) {
+        reverse.push(cur.val);
+        cur = cur.next;
+    }
+    // console.log(reverse);
 
-    //     return original.every((val, i) => val === reverse[i]);
+    return original.every((val, i) => val === reverse[i]);
 
     // solution 2 - without converting to arrays to check (this is faster than other solutions)
     //find size of linked list
@@ -80,5 +81,37 @@ var isPalindrome = function(head) {
             return false;
         }
     }
+    return true;
+};
+
+// solution 3 - optimized solution 1
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    let arr = [];
+    let node = head;
+    while (node !== null) {
+        arr.push(node.val);
+        node = node.next;
+    }
+
+    let arr2 = [...arr].reverse(); // instead of reversing linked list, just reverse the array, both are O(N) in time complexity;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== arr2[i]) {
+            return false;
+        }
+    }
+
+    // console.log(arr)
+    // console.log(arr2);
     return true;
 };
