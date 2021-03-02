bfcc73843a087db758c7f7b1b87e6e10b5c68027
asd0999 <obelixotic@gmail.com>
2021-03-01 23:57:41 -0500

    #240 - search 2D matrix II

---
+++ 240_search_2D_matrix_2.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[0].length; j++) {
+            if (matrix[i][j] === target) {
+                return true;
+            }
+        }
+    }
+    return false;
+};
+
+// test :
+console.log(
+    searchMatrix(
+        [
+            [2, 5, 8, 12, 19],
+            [3, 6, 9, 16, 22],
+            [10, 13, 14, 17, 24],
+            [18, 21, 23, 26, 30],
+        ],
+        5
+    )
+);
+console.log(searchMatrix([
+    [-1, 3]
+], 3));
