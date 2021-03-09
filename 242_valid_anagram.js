6f7a65f87352c8852b7aa684edb52d61e192f14c
asd0999 <obelixotic@gmail.com>
2021-03-08 23:52:19 -0500

    #242 - valid anagram

--- 242_valid_anagram.js
+++ 242_valid_anagram.js
@@ -6,7 +6,7 @@
 var isAnagram = function(s, t) {
     let a = s.split("").sort().join("");
     let b = t.split("").sort().join("");
-    console.log(a, b);
+    // console.log(a, b);
 
     return a === b;
 };
