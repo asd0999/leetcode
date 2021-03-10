8fcac38d77c4199e31b26a0d580d00029a2e743b
asd0999 <obelixotic@gmail.com>
2021-03-09 23:45:28 -0500

    #98 - valid BT

---
+++ 98_validate_BT.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function(root) {
+    return validate(root, null, null);
+};
+
+var validate = function(root, min, max) {
+    if (!root) {
+        return true;
+    } else if (
+        (max !== null && root.val >= max) ||
+        (min !== null && root.val <= min)
+    ) {
+        return false;
+    } else {
+        return (
+            validate(root.left, min, root.val) && validate(root.right, root.val, max)
+        );
+    }
+};
