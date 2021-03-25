b8ce75aa07da0f371059830184006f3a64557f5a
asd0999 <obelixotic@gmail.com>
2021-03-24 23:08:26 -0400

    #557 - reverse words in a string

--- 557_reverse_words_in_string.js
+++ 557_reverse_words_in_string.js
@@ -6,4 +6,4 @@ var reverseWords = function(s) {
     return s.split("").reverse().join("").split(" ").reverse().join(" ");
 };
 
-console.log(reverseWords("Let's take LeetCode contest"));
+console.log(reverseWords("Let's take LeetCode contest")); // 's\'teL ekat edoCteeL tsetnoc'
